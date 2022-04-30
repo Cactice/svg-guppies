ea8057e2cb801049af0bec84cef498397a4f04c3
cactice <14835424+Cactice@users.noreply.github.com>
2022-04-30 13:58:34 +0900

    Refactor fill

--- usvg_layout/src/iterator/fill.rs
+++ usvg_layout/src/iterator/fill.rs
@@ -41,20 +41,19 @@ pub fn is_convex(polygon: Vec<DVec2>) -> bool {
     if polygon.len() < 3 {
         return false;
     }
-    let N = polygon.len() - 1;
 
-    let mut wSign = 0.0; // First nonzero orientation (positive or negative)
+    let mut w_sign = 0.0; // First nonzero orientation (positive or negative)
 
-    let mut xSign = 0.0;
-    let mut xFirstSign = 0.0; // Sign of first nonzero edge vector x
-    let mut xFlips = 0.0; // Number of sign changes in x
+    let mut x_sign = 0.0;
+    let mut x_flips = 0.0; // Number of sign changes in x
 
-    let mut ySign = 0.0;
-    let mut yFirstSign = 0.0; // Sign of first nonzero edge vector y
-    let mut yFlips = 0.0; // Number of sign changes in y
+    let mut y_sign = 0.0;
+    let mut y_flips = 0.0; // Number of sign changes in y
 
-    let mut curr = polygon[N - 1]; // Second-to-last vertex
-    let mut next = polygon[N]; // Last vertex
+    let mut curr = polygon[polygon.len() - 2]; // Second-to-last vertex
+    let mut next = polygon[polygon.len() - 1]; // Last vertex
+    let x_first_sign = if next.x - curr.x > 0.0 { -1.0 } else { 1.0 }; // Sign of first nonzero edge vector x
+    let y_first_sign = if next.y - curr.y > 0.0 { -1.0 } else { 1.0 }; // Sign of first nonzero edge vector y
 
     for v in polygon {
         // Each vertex, in order
@@ -70,67 +69,49 @@ pub fn is_convex(polygon: Vec<DVec2>) -> bool {
         let ax = next.x - curr.x;
         let ay = next.y - curr.y;
 
-        // Calculate sign flips using the next edge vector ("after"),
-        // recording the first sign.
-        if ax > 0.0 {
-            if xSign == 0.0 {
-                xFirstSign = 1.0;
-            } else if xSign < 0.0 {
-                xFlips += 1.0
-            }
-            xSign = 1.0
-        } else if ax < 0.0 {
-            if xSign == 0.0 {
-                xFirstSign = -1.0
-            } else if xSign > 0.0 {
-                xFlips += 1.0
-            }
-            xSign = -1.0
-        }
+        let next_x_sign = if ax > 0.0 { 1.0 } else { -1.0 };
+        x_flips += if x_sign * -next_x_sign < 0.0 {
+            1.0
+        } else {
+            0.0
+        };
+        x_sign = next_x_sign;
 
-        if xFlips > 2.0 {
+        if x_flips > 2.0 {
             return false;
         }
 
-        if ay > 0.0 {
-            if ySign == 0.0 {
-                yFirstSign = 1.0
-            } else if ySign < 0.0 {
-                yFlips += 1.0
-            }
-            ySign = 1.0
-        } else if ay < 0.0 {
-            if ySign == 0.0 {
-                yFirstSign = -1.0
-            } else if ySign > 0.0 {
-                yFlips += 1.0
-            }
-            ySign = -1.0
-        }
+        let next_y_sign = if ay > 0.0 { 1.0 } else { -1.0 };
+        y_flips += if y_sign * -next_y_sign < 0.0 {
+            1.0
+        } else {
+            0.0
+        };
+        y_sign = next_y_sign;
 
-        if yFlips > 2.0 {
+        if y_flips > 2.0 {
             return false;
         }
 
         // Find out the orientation of this pair of edges,
         // && ensure it does not differ from previous ones.
         let w = bx * ay - ax * by;
-        if (wSign == 0.0) && (w != 0.0) {
-            wSign = w
-        } else if (wSign > 0.0) && (w < 0.0) {
+        if (w_sign == 0.0) && (w != 0.0) {
+            w_sign = w
+        } else if (w_sign > 0.0) && (w < 0.0) {
             return false;
-        } else if (wSign < 0.0) && (w > 0.0) {
+        } else if (w_sign < 0.0) && (w > 0.0) {
             return false;
         }
     }
-    if (xSign != 0.0) && (xFirstSign != 0.0) && (xSign != xFirstSign) {
-        xFlips += 1.0
+    if (x_sign != 0.0) && (x_first_sign != 0.0) && (x_sign != x_first_sign) {
+        x_flips += 1.0
     }
-    if (ySign != 0.0) && (yFirstSign != 0.0) && (ySign != yFirstSign) {
-        yFlips += 1.0
+    if (y_sign != 0.0) && (y_first_sign != 0.0) && (y_sign != y_first_sign) {
+        y_flips += 1.0
     }
     // Concave polygons have two sign flips along each axis.
-    if (xFlips != 2.0) || (yFlips != 2.0) {
+    if (x_flips != 2.0) || (y_flips != 2.0) {
         return false;
     }
 
