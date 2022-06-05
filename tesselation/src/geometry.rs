41cebf008cb24a8270e37476f9b0513637d5811a
cactice <14835424+Cactice@users.noreply.github.com>
2022-06-05 18:42:59 +0900

    Add svg_set

--- tesselation/src/geometry.rs
+++ tesselation/src/geometry.rs
@@ -1,9 +1,9 @@
 use crate::{fill::iterate_fill, stroke::iterate_stroke};
 use glam::{DVec2, Vec2, Vec4};
 use lyon::lyon_tessellation::{FillVertex, StrokeVertex, VertexBuffers};
-
-use std::{collections::HashMap, ops::Range};
-use usvg::{Node, Path};
+use roxmltree::{Document, NodeId};
+use std::{collections::HashMap, ops::Range, sync::Arc};
+use usvg::{fontdb::Source, Node, NodeKind, Path, Tree};
 pub type Index = u32;
 pub type Vertices = Vec<Vertex>;
 pub type Indices = Vec<Index>;
@@ -13,6 +13,7 @@ pub type Position = Vec2;
 pub type Rect = (Position, Size);
 
 pub const FALLBACK_COLOR: Vec4 = Vec4::ONE;
+
 #[derive(PartialEq, PartialOrd, Eq, Ord, Clone, Copy, Debug)]
 pub enum IndicesPriority {
     Fixed,
@@ -205,6 +206,75 @@ impl Geometry {
             indices: v.indices,
             index_base,
             ..Default::default()
+        }
+    }
+}
+
+fn recursive_svg(
+    node: usvg::Node,
+    parent_priority: IndicesPriority,
+    callback: &mut Callback,
+    geometry_set: &mut GeometrySet,
+    mut ids: Vec<String>,
+) {
+    let priority = parent_priority.max(callback.process_events(&node));
+    let node_ref = &node.borrow();
+    let id = NodeKind::id(node_ref);
+    if !id.is_empty() {
+        ids.push(id.to_string());
+    }
+
+    if let usvg::NodeKind::Path(ref p) = *node.borrow() {
+        let geometry = Geometry::new(p, geometry_set.get_vertices_len(priority), ids.to_vec());
+        geometry_set.push_with_priority(geometry, priority);
+    }
+    for child in node.children() {
+        recursive_svg(child, priority, callback, geometry_set, ids.clone());
+    }
+}
+#[derive(Clone, Debug)]
+pub struct SvgSet {
+    pub geometry_set: GeometrySet,
+    pub root: Node,
+    pub id_map: HashMap<String, NodeId>,
+    pub bbox: Rect,
+}
+impl SvgSet {
+    pub fn new(xml: &str, mut callback: Callback) -> Self {
+        let font = include_bytes!("../fallback_font/Roboto-Medium.ttf");
+        let mut opt = usvg::Options::default();
+        opt.fontdb
+            .load_font_source(Source::Binary(Arc::new(font.as_ref())));
+        opt.font_family = "Roboto Medium".to_string();
+        opt.keep_named_groups = true;
+        let mut geometry_set = GeometrySet::default();
+        let tree = Document::parse(xml).unwrap();
+        let rtree = Tree::from_xmltree(&tree, &opt.to_ref()).unwrap();
+        let id_map = tree
+            .descendants()
+            .fold(HashMap::<String, NodeId>::new(), |mut acc, curr| {
+                if let Some(attribute_id) = tree.root().attribute("id") {
+                    acc.insert(attribute_id.to_string(), curr.id());
+                }
+                acc
+            });
+        recursive_svg(
+            rtree.root(),
+            IndicesPriority::Fixed,
+            &mut callback,
+            &mut geometry_set,
+            vec![],
+        );
+        let view_box = rtree.svg_node().view_box;
+        let bbox: Rect = (
+            Vec2::new(view_box.rect.x() as f32, view_box.rect.y() as f32),
+            Vec2::new(view_box.rect.width() as f32, view_box.rect.height() as f32),
+        );
+        Self {
+            geometry_set,
+            root: rtree.root(),
+            id_map,
+            bbox,
         }
     }
 }
--- tesselation/src/lib.rs
+++ tesselation/src/lib.rs
@@ -2,11 +2,11 @@ mod convert_path;
 mod fill;
 pub mod geometry;
 mod stroke;
-use geometry::{Callback, DrawPrimitives, Geometry, GeometrySet, IndicesPriority, Rect};
+use geometry::{Callback, DrawPrimitives, Geometry, GeometrySet, IndicesPriority, Rect, SvgSet};
 pub use glam;
 use glam::{DMat4, Vec2};
 
-use roxmltree::{NodeId};
+use roxmltree::NodeId;
 use std::{collections::HashMap, sync::Arc};
 pub use usvg;
 use usvg::{fontdb::Source, Node, NodeKind, Tree};
@@ -16,73 +16,15 @@ struct TransformVariable {
     transform_index: u16,
 }
 
-#[derive(Clone, Debug)]
-struct SvgSet {
-    geometry_set: GeometrySet,
-    root: Node,
-    id_map: HashMap<String, NodeId>,
-}
-
-fn recursive_svg(
-    node: usvg::Node,
-    parent_priority: IndicesPriority,
-    callback: &mut Callback,
-    geometry_set: &mut GeometrySet,
-    mut ids: Vec<String>,
-) {
-    let priority = parent_priority.max(callback.process_events(&node));
-    let node_ref = &node.borrow();
-    let id = NodeKind::id(node_ref);
-    if !id.is_empty() {
-        ids.push(id.to_string());
-    }
-
-    if let usvg::NodeKind::Path(ref p) = *node.borrow() {
-        let geometry = Geometry::new(p, geometry_set.get_vertices_len(priority), ids.to_vec());
-        geometry_set.push_with_priority(geometry, priority);
-    }
-    for child in node.children() {
-        recursive_svg(child, priority, callback, geometry_set, ids.clone());
-    }
-}
-
-pub fn init(mut callback: Callback) -> (DrawPrimitives, Rect) {
+pub fn init(callback: Callback) -> (DrawPrimitives, Rect) {
     // Parse and tessellate the geometry
 
-    let contents = include_bytes!("../fallback_font/Roboto-Medium.ttf");
-    let mut opt = usvg::Options::default();
-    opt.fontdb
-        .load_font_source(Source::Binary(Arc::new(contents.as_ref())));
-    opt.font_family = "Roboto Medium".to_string();
-    opt.keep_named_groups = true;
-
-    let mut geometry_set = GeometrySet::default();
-    let tree = roxmltree::Document::parse(include_str!("../../svg/life_text.svg")).unwrap();
-    let rtree = Tree::from_xmltree(&tree, &opt.to_ref()).unwrap();
-    let _id_map = tree
-        .descendants()
-        .fold(HashMap::<String, NodeId>::new(), |mut acc, curr| {
-            if let Some(attribute_id) = tree.root().attribute("id") {
-                acc.insert(attribute_id.to_string(), curr.id());
-            }
-            acc
-        });
-    recursive_svg(
-        rtree.root(),
-        IndicesPriority::Fixed,
-        &mut callback,
-        &mut geometry_set,
-        vec![],
-    );
-
-    let view_box = rtree.svg_node().view_box;
-    let rect: Rect = (
-        Vec2::new(view_box.rect.x() as f32, view_box.rect.y() as f32),
-        Vec2::new(view_box.rect.width() as f32, view_box.rect.height() as f32),
-    );
-
+    let svg_set = SvgSet::new(include_str!("../../svg/life_text.svg"), callback);
     (
-        (geometry_set.get_vertices(), geometry_set.get_indices()),
-        rect,
+        (
+            svg_set.geometry_set.get_vertices(),
+            svg_set.geometry_set.get_indices(),
+        ),
+        svg_set.bbox,
     )
 }
