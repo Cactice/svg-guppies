d0cda0f4572ac74685484f2dfd4865b05efdd8e4
cactice <14835424+Cactice@users.noreply.github.com>
2022-06-16 10:23:54 +0900

    Utilize StartCause::Init

--- windowing/src/lib.rs
+++ windowing/src/lib.rs
@@ -3,14 +3,13 @@ use setup::Setup;
 pub use tesselation;
 use tesselation::callback::InitCallback;
 use tesselation::geometry::Rect;
-use tesselation::glam::{Mat4, Vec2, Vec3};
+use tesselation::glam::{Mat4, Vec2};
 use tesselation::init;
 use winit::dpi::PhysicalSize;
 use winit::window::WindowBuilder;
 use winit::{
     event::{Event, WindowEvent},
     event_loop::{ControlFlow, EventLoop},
-    window::Window,
 };
 
 fn get_scale(size: PhysicalSize<u32>, svg_scale: Vec2) -> Mat4 {
@@ -26,32 +25,65 @@ fn get_scale(size: PhysicalSize<u32>, svg_scale: Vec2) -> Mat4 {
     )
 }
 
-async fn run(event_loop: EventLoop<()>, window: Window, callback: InitCallback<'_>) {
+pub fn main(callback: InitCallback) {
+    let event_loop = EventLoop::new();
     let svg_set = init(callback);
     let vertices = svg_set.geometry_set.get_vertices();
     let indices = svg_set.geometry_set.get_indices();
     let Rect {
         size: svg_scale, ..
     } = svg_set.bbox;
-    let win_size = window.inner_size();
-    let mut translate = Mat4::from_translation([-1., 1.0, 0.0].into());
-    let mut scale = get_scale(win_size, svg_scale);
-    let transform: Mat4 = translate * scale;
-    let Setup {
-        mut redraw,
-        adapter,
-        instance,
-        pipeline_layout,
-        shader,
-    } = Setup::new(&window, transform, &vertices, &indices).await;
-    event_loop.run(move |event, _, control_flow| {
-        // Have the closure take ownership of the resources.
-        // `event_loop.run` never returns, therefore we must do this to ensure
-        // the resources are properly cleaned up.
-        let _ = (&instance, &adapter, &shader, &pipeline_layout);
+    let mut translate = Mat4::IDENTITY;
+    let mut scale = Mat4::IDENTITY;
+    let mut redraw = None;
+    let mut window = None;
 
+    event_loop.run(move |event, event_loop, control_flow| {
         *control_flow = ControlFlow::Wait;
         match event {
+            Event::NewEvents(start_cause) => match start_cause {
+                winit::event::StartCause::Init => {
+                    window = Some(
+                        WindowBuilder::new()
+                            .with_title("SVG-GUI")
+                            .build(&event_loop)
+                            .unwrap(),
+                    );
+                    let window = window.as_ref().expect("Window is None");
+                    #[cfg(target_arch = "wasm32")]
+                    {
+                        std::panic::set_hook(Box::new(console_error_panic_hook::hook));
+                        use winit::platform::web::WindowExtWebSys;
+
+                        web_sys::window()
+                            .and_then(|win| win.document())
+                            .and_then(|doc| doc.body())
+                            .and_then(|body| {
+                                body.remove_child(&body.last_element_child().unwrap())
+                                    .unwrap();
+                                body.append_child(&web_sys::Element::from(window.canvas()))
+                                    .ok()
+                            })
+                            .expect("Couldn't append canvas to document body");
+                    }
+                    let transform: Mat4 = translate * scale;
+                    let setup =
+                        pollster::block_on(Setup::new(&window, transform, &vertices, &indices));
+                    let Setup {
+                        redraw: redraw0,
+                        adapter,
+                        instance,
+                        pipeline_layout,
+                        shader,
+                    } = setup;
+                    redraw = Some(redraw0);
+
+                    let win_size = window.inner_size();
+                    translate = Mat4::from_translation([-1., 1.0, 0.0].into());
+                    scale = get_scale(win_size, svg_scale);
+                }
+                _ => {}
+            },
             Event::WindowEvent {
                 event: WindowEvent::MouseWheel { delta, .. },
                 ..
@@ -67,13 +99,17 @@ async fn run(event_loop: EventLoop<()>, window: Window, callback: InitCallback<'
                 event: WindowEvent::Resized(size),
                 ..
             } => {
-                Setup::resize(size, &redraw.device, &redraw.surface, &mut redraw.config);
-                scale = get_scale(size, svg_scale);
+                if let Some(redraw) = redraw.as_mut() {
+                    Setup::resize(size, &redraw.device, &redraw.surface, &mut redraw.config);
+                    scale = get_scale(size, svg_scale);
+                }
             }
             Event::RedrawRequested(_) => {
-                redraw.transform = translate * scale;
-                Setup::redraw(&redraw, [Mat4::IDENTITY; 2048].to_vec());
-                window.request_redraw();
+                if let (Some(mut redraw), Some(window)) = (redraw.as_mut(), window.as_mut()) {
+                    redraw.transform = translate * scale;
+                    Setup::redraw(&redraw, [Mat4::IDENTITY; 2048].to_vec());
+                    window.request_redraw();
+                }
             }
             Event::WindowEvent {
                 event: WindowEvent::CloseRequested,
@@ -82,29 +118,4 @@ async fn run(event_loop: EventLoop<()>, window: Window, callback: InitCallback<'
             _ => {}
         }
     });
-}
-
-pub fn main(callback: InitCallback) {
-    let event_loop = EventLoop::new();
-    let window = WindowBuilder::new()
-        .with_title("SVG-GUI")
-        .build(&event_loop)
-        .unwrap();
-
-    #[cfg(target_arch = "wasm32")]
-    {
-        std::panic::set_hook(Box::new(console_error_panic_hook::hook));
-        use winit::platform::web::WindowExtWebSys;
-        web_sys::window()
-            .and_then(|win| win.document())
-            .and_then(|doc| doc.body())
-            .and_then(|body| {
-                body.remove_child(&body.last_element_child().unwrap())
-                    .unwrap();
-                body.append_child(&web_sys::Element::from(window.canvas()))
-                    .ok()
-            })
-            .expect("Couldn't append canvas to document body");
-    }
-    pollster::block_on(run(event_loop, window, callback));
 }
