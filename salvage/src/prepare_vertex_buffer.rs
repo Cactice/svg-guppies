b8899aec0c7384f61f6d1fbd3a1cedb4756f9470
cactice <14835424+Cactice@users.noreply.github.com>
2022-08-07 15:44:06 +0900

    Rm unused imports

--- salvage/src/prepare_vertex_buffer.rs
+++ salvage/src/prepare_vertex_buffer.rs
@@ -1,16 +1,10 @@
-use crate::{
-    callback::{IndicesPriority, InitCallback},
-    fill::iterate_fill,
-    stroke::iterate_stroke,
-};
+use crate::{fill::iterate_fill, stroke::iterate_stroke};
 use guppies::{
-    glam::{Vec2, Vec4},
-    primitives::{Index, Indices, Rect, Vertex, Vertices},
+    glam::Vec4,
+    primitives::{Index, Vertex},
 };
 use lyon::lyon_tessellation::VertexBuffers;
-use roxmltree::{Document, NodeId};
-use std::{collections::HashMap, iter, ops::Range, sync::Arc};
-use usvg::{fontdb::Source, NodeKind, Options, Path, PathBbox, Tree};
+use usvg::Path;
 pub const FALLBACK_COLOR: Vec4 = Vec4::ONE;
 
 pub fn prepare_vertex_buffer(p: &Path, transform_id: u32) -> VertexBuffers<Vertex, Index> {
