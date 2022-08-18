3ae11848ff1d865fefd50cf4dfdc8e6168ea20ba
cactice <14835424+Cactice@users.noreply.github.com>
2022-08-18 22:23:27 +0900

    Rename lib_main

--- life/src/lib.rs
+++ life/src/lib.rs
@@ -1,8 +1,8 @@
 mod main;
-use main::main as lib_main;
+use main::main as main_main;
 use mobile_entry_point::mobile_entry_point;
 
 #[mobile_entry_point]
 fn mobile_main() {
-    lib_main()
+    main_main()
 }
