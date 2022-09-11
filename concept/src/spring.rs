ee6f548550859bd8fc141bb4d71aa034a671e51f
cactice <14835424+Cactice@users.noreply.github.com>
2022-09-11 15:29:24 +0900

    Undo changes in spring.rs

--- concept/src/spring.rs
+++ concept/src/spring.rs
@@ -1,21 +1,21 @@
 use guppies::glam::Mat4;
 use natura::{AngularFrequency, DampingRatio, DeltaTime, Spring};
-use std::cell::RefCell;
 use std::default::Default;
 use std::iter::zip;
 use std::rc::Rc;
+pub type GetSelf<T> = Rc<dyn Fn(&mut T) -> &mut SpringMat4<T>>;
 
-pub struct SpringMat4<'a> {
+pub struct SpringMat4<T> {
     spring: Spring,
     target: Mat4,
-    pub current: &'a mut Mat4,
+    pub current: Mat4,
     velocity: Mat4,
     pub is_animating: bool,
-    on_complete: Rc<RefCell<dyn FnMut() -> ()>>,
+    on_complete: Rc<dyn Fn(&mut T) -> ()>,
 }
 
-impl<'a> SpringMat4<'a> {
-    pub fn new<'b: 'a>(current: &'b mut Mat4) -> Self {
+impl<T> Default for SpringMat4<T> {
+    fn default() -> Self {
         Self {
             spring: Spring::new(
                 DeltaTime(natura::fps(60)),
@@ -23,52 +23,61 @@ impl<'a> SpringMat4<'a> {
                 DampingRatio(0.7),
             ),
             is_animating: false,
-            current,
+            current: Default::default(),
             target: Default::default(),
             velocity: Default::default(),
-            on_complete: Rc::new(RefCell::new(|| {})),
+            on_complete: Rc::new(|_| {}),
         }
     }
+}
+
+impl<T> SpringMat4<T> {
     pub fn spring_to(
-        mut self,
-        register: &mut Vec<Self>,
+        ctx: &mut T,
+        mut get_self: GetSelf<T>,
+        register: Rc<dyn Fn(&mut T, GetSelf<T>) -> ()>,
         target: Mat4,
-        on_complete: Rc<RefCell<dyn FnMut() -> ()>>,
+        on_complete: Rc<dyn Fn(&mut T) -> ()>,
     ) {
-        self.is_animating = true;
-        self.target = target;
-        self.target = target;
-        self.on_complete = on_complete;
-        self.update();
-        register.push(self);
+        {
+            let mut me = get_self(ctx);
+            me.is_animating = true;
+            me.target = target;
+            me.on_complete = on_complete;
+        }
+        Self::update(ctx, &mut get_self);
+        register(ctx, get_self);
     }
 
-    pub fn update(&mut self) -> bool {
+    pub fn update(ctx: &mut T, get_self: &mut GetSelf<T>) -> bool {
         let mut current_position_vec = vec![];
         let mut vel_vec = vec![];
 
         let animating_complete = {
+            let mut me = get_self(ctx);
             zip(
-                zip(self.current.to_cols_array(), self.velocity.to_cols_array()),
-                self.target.to_cols_array(),
+                zip(me.current.to_cols_array(), me.velocity.to_cols_array()),
+                me.target.to_cols_array(),
             )
             .for_each(|((current_position, vel), target)| {
                 let (new_current_position, new_vel) =
-                    self.spring
+                    me.spring
                         .update(current_position as f64, vel as f64, target as f64);
                 current_position_vec.push(new_current_position as f32);
                 vel_vec.push(new_vel as f32);
             });
-            *self.current = Mat4::from_cols_array(&current_position_vec.try_into().unwrap());
-            self.velocity = Mat4::from_cols_array(&vel_vec.try_into().unwrap());
+            me.current = Mat4::from_cols_array(&current_position_vec.try_into().unwrap());
+            me.velocity = Mat4::from_cols_array(&vel_vec.try_into().unwrap());
 
-            self.current.abs_diff_eq(self.target, 1.0)
-                && self.velocity.abs_diff_eq(Mat4::ZERO, 100.0)
+            me.current.abs_diff_eq(me.target, 1.0) && me.velocity.abs_diff_eq(Mat4::ZERO, 100.0)
         };
         if animating_complete {
-            self.is_animating = false;
-            let mut x = self.on_complete.try_borrow_mut().unwrap();
-            x()
+            let call = {
+                let me = get_self(ctx);
+                me.is_animating = false;
+                me.on_complete.clone()
+            };
+            call(ctx);
         }
         animating_complete
     }
