3c0108493dc4f6da40861b1361b963222b34c746
cactice <14835424+Cactice@users.noreply.github.com>
2022-11-12 16:13:33 +0900

    Add default implementation for PassDown

--- concept/src/uses.rs
+++ concept/src/uses.rs
@@ -4,15 +4,8 @@ use crate::svg_init::get_default_init_callback;
 
 pub fn use_svg<C: FnMut(Node, PassDown)>(xml: &str, mut callback: C) -> SvgSet {
     let mut default_callback = get_default_init_callback();
-    SvgSet::new(
-        xml,
-        PassDown {
-            transform_id: 1,
-            ..Default::default()
-        },
-        |node, passdown| {
-            callback(node.clone(), passdown);
-            default_callback(node, passdown)
-        },
-    )
+    SvgSet::new(xml, PassDown::default(), |node, passdown| {
+        callback(node.clone(), passdown);
+        default_callback(node, passdown)
+    })
 }
