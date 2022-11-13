8159d8c50d0f634e3fb01966f5663084ee84eea0
cactice <14835424+Cactice@users.noreply.github.com>
2022-11-13 17:51:31 +0900

    Execute scraping at the beginning only

--- examples/layout/src/main.rs
+++ examples/layout/src/main.rs
@@ -2,17 +2,25 @@ mod call_back;
 mod constraint;
 
 use bytemuck::cast_slice;
-use call_back::{get_x_constraint, get_y_constraint};
-use concept::svg_init::get_default_init_callback;
-use constraint::Constraint;
-use guppies::{glam::Mat4, primitives::Rect, winit::dpi::PhysicalSize};
-use mobile_entry_point::mobile_entry_point;
-use salvage::{
-    callback::PassDown,
-    svg_set::SvgSet,
-    usvg::{self, NodeExt},
+use call_back::get_constraint;
+use concept::{
+    svg_init::{regex::Regex, TRANSFORM_REGEX},
+    uses::use_svg,
 };
-use std::vec;
+use constraint::Constraint;
+use guppies::{
+    glam::{Mat4, Vec3},
+    primitives::Rect,
+    winit::dpi::PhysicalSize,
+};
+use mobile_entry_point::mobile_entry_point;
+use salvage::usvg::{self, NodeExt};
+use std::{collections::HashMap, vec};
+
+struct Layout {
+    constraint: Constraint,
+    bbox: Mat4,
+}
 
 fn get_svg_size(svg_scale: Rect) -> Mat4 {
     Mat4::from_scale([svg_scale.size.x as f32, svg_scale.size.y as f32, 1.].into())
@@ -22,55 +30,67 @@ fn get_screen_size(size: PhysicalSize<u32>) -> Mat4 {
     Mat4::from_scale([size.width as f32, size.height as f32, 1.].into())
 }
 
-fn layout_recursively(svg: Mat4, display: Mat4, node: usvg::Node, parent: Mat4) -> Vec<Mat4> {
-    let mut children_transforms: Vec<Mat4> = node
-        .children()
-        .into_iter()
-        .flat_map(|child| layout_recursively(svg, display, child, parent))
-        .collect();
+fn get_layout(node: &usvg::Node) -> Option<Layout> {
+    let transform_regex = Regex::new(TRANSFORM_REGEX).unwrap();
+    let id = node.id();
+    if transform_regex.is_match(&id) {
+        if let Some(bbox) = node.calculate_bbox() {
+            let bbox_mat4 = Mat4::from_scale_rotation_translation(
+                [bbox.width() as f32, bbox.height() as f32, 0.].into(),
+                Default::default(),
+                [bbox.x() as f32, bbox.y() as f32, 0.].into(),
+            );
+            let constraint = get_constraint(&id);
 
-    if let Some(bbox) = node.calculate_bbox() {
-        let id = node.id();
-        let constraint = Constraint {
-            x: get_x_constraint(&id),
-            y: get_y_constraint(&id),
-        };
-        let bbox_mat4 = Mat4::from_scale_rotation_translation(
-            [bbox.width() as f32, bbox.height() as f32, 0.].into(),
-            Default::default(),
-            [bbox.x() as f32, bbox.y() as f32, 0.].into(),
-        );
-        if node.id().contains("#transform") {
-            children_transforms.insert(0, constraint.to_mat4(display, svg, bbox_mat4));
+            return Some(Layout {
+                constraint: constraint,
+                bbox: bbox_mat4,
+            });
         }
     }
-    children_transforms
+    None
 }
 
 pub fn main() {
-    let svg_set = SvgSet::new(
-        include_str!("../MenuBar.svg"),
-        PassDown::default(),
-        get_default_init_callback(),
-    );
-    guppies::render_loop(move |event, gpu_redraw| {
-        if let guppies::winit::event::Event::WindowEvent {
-            event: guppies::winit::event::WindowEvent::Resized(p),
-            ..
-        } = event
-        {
-            let display = get_screen_size(*p);
-            let svg = get_svg_size(svg_set.bbox);
+    let mut svg_id_to_transform_id = HashMap::<String, usize>::new();
+    let mut transform_count = 1;
+    let mut layouts = Vec::new();
 
-            let mut transforms = vec![Mat4::IDENTITY, Mat4::IDENTITY];
-            transforms.append(&mut layout_recursively(
-                svg,
-                display,
-                svg_set.root.clone(),
-                svg,
-            ));
-            gpu_redraw.update_texture([cast_slice(&transforms[..])].concat());
-            gpu_redraw.update_triangles(svg_set.get_combined_geometries().triangles, 0);
+    let svg_set = use_svg(include_str!("../MenuBar.svg"), |node, pass_down| {
+        let id = node.id();
+        let transform_regex = Regex::new(TRANSFORM_REGEX).unwrap();
+        if transform_regex.is_match(&id) {
+            transform_count += 1;
+            svg_id_to_transform_id.insert(id.to_string(), transform_count);
+            if let Some(layout) = get_layout(&node) {
+                layouts.push(layout);
+            }
+        }
+    });
+
+    guppies::render_loop(move |event, gpu_redraw| {
+        if let guppies::winit::event::Event::WindowEvent { event, .. } = event {
+            match event {
+                guppies::winit::event::WindowEvent::Resized(p) => {
+                    let display = get_screen_size(*p);
+                    let svg = get_svg_size(svg_set.bbox);
+                    let mut transforms = vec![Mat4::IDENTITY, Mat4::IDENTITY];
+                    transforms.append(
+                        &mut layouts
+                            .iter()
+                            .map(|layout| layout.constraint.to_mat4(display, svg, layout.bbox))
+                            .collect(),
+                    );
+                    gpu_redraw.update_texture([cast_slice(&transforms[..])].concat());
+                    gpu_redraw.update_triangles(svg_set.get_combined_geometries().triangles, 0);
+                }
+
+                guppies::winit::event::WindowEvent::Touch(touch) => {
+                    let touch = Vec3::from([touch.location.x as f32, touch.location.y as f32, 0.]);
+                    let touch_is_on_node = todo!();
+                }
+                _ => {}
+            }
         }
     });
 }
