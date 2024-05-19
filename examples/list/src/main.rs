c0e9e42ddfc68d8abc97da00f4568834f62c1d1c
cactice <14835424+Cactice@users.noreply.github.com>
2024-05-19 18:26:41 +0900

    Remove adjust

--- examples/list/src/main.rs
+++ examples/list/src/main.rs
@@ -1,7 +1,9 @@
-use experiment::responsive::layout_machine::{self, ConstraintMap};
+use experiment::responsive::constraint::YConstraint;
+use experiment::responsive::layout_machine::ConstraintMap;
 use experiment::serde_json;
 use experiment::{responsive::layout_machine::LayoutMachine, uses::use_svg};
 use guppies::bytemuck::cast_slice;
+use guppies::glam::Mat4;
 use guppies::{GpuRedraw, Guppy};
 use mobile_entry_point::mobile_entry_point;
 
@@ -20,20 +22,43 @@ pub fn main() {
         None,
         None,
     );
+    let var_name = transform_id.clone();
+    let container_name = "ComponentBox #transform #layout".to_owned();
+    let mut x = layout_machine
+        .id_to_layout
+        .get(&container_name)
+        .cloned()
+        .unwrap();
+    x.constraint.y = match x.constraint.y {
+        YConstraint::Top(x) => YConstraint::Top(x + 30.0),
+        y => y,
+    };
     let list = use_svg(
         include_str!("../V2.svg").to_string(),
         |node, mut pass_down| {
             layout_machine.add_node(&node, &mut pass_down);
+            transform_id = transform_id.max(pass_down.transform_id);
         },
-        Some("ComponentBox #transform #layout #component".to_string()),
-        Some(transform_id),
+        Some((
+            "ListItem #transform #layout #component".to_string(),
+            Some(container_name.clone()),
+        )),
+        Some(var_name),
     );
+    layout_machine
+        .id_to_layout
+        .insert(container_name.clone(), x);
 
     let mut guppy = Guppy::new([GpuRedraw::default()]);
 
     guppy.register(move |event, gpu_redraws| {
         layout_machine.event_handler(event);
-        gpu_redraws[0].update_texture([cast_slice(&layout_machine.transforms[..])].concat());
+        gpu_redraws[0].update_texture(
+            [cast_slice(
+                &[layout_machine.transforms.clone()].concat()[..],
+            )]
+            .concat(),
+        );
         gpu_redraws[0].update_triangles(
             list.get_combined_geometries()
                 .extend(&svg_set.get_combined_geometries())
--- experiment/src/uses.rs
+++ experiment/src/uses.rs
@@ -5,15 +5,20 @@ use crate::svg_init::{get_default_init_callback, PassDown};
 pub fn use_svg<C: FnMut(&Node, &mut PassDown)>(
     xml: String,
     mut callback: C,
-    component: Option<String>,
-    default_callback_args: Option<u32>,
+    component_and_destination: Option<(String, Option<String>)>,
+    transform_count: Option<u32>,
 ) -> SvgSet {
     let initial_pass_down = PassDown {
-        is_include: component.is_none(),
+        is_include: component_and_destination.is_none(),
+        parent: component_and_destination
+            .clone()
+            .map(|component_and_destination| component_and_destination.1)
+            .flatten(),
         ..Default::default()
     };
-    let args = default_callback_args.unwrap_or(1);
-    let mut default_callback = get_default_init_callback(args, component);
+    let transform_count = transform_count.unwrap_or(1);
+    let mut default_callback =
+        get_default_init_callback(transform_count, component_and_destination.map(|x| x.0));
     SvgSet::new(xml.to_string(), initial_pass_down, |node, passdown| {
         let (geometry, mut passdown) = default_callback(node.clone(), passdown);
         callback(&node, &mut passdown);
